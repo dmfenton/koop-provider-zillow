438f1437657b75a19dab79c1a8247f2a98a72619
dmfenton <dfenton@esri.com>
2015-03-19 22:57:13 -0400

    edited name in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // the name of provider is used by koop to help build default routes for FeatureService and a preview
-exports.name = 'sample';
+exports.name = 'zillow';
 
 // the "pattern" is used to build routes and is optional 
 // a pattern is essentially the default route pattern for the provider 
@@ -12,4 +12,4 @@ exports.controller = require('./controller');
 exports.routes = require('./routes');
 
 // attaches the model to the provider to be passed into the controller at start up time 
-exports.model = require('./models/Sample.js');  
+exports.model = require('./models/Sample.js');
