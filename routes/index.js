f8e010b9adcc02df49f694c17fb05345acc11661
dmfenton <dfenton@esri.com>
2015-03-26 19:56:10 -0400

    dynamically generate route combinations

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,52 @@
 // Defines the routes and params name that will be passed in req.params 
 // routes tell Koop what controller method should handle what request route
 
-module.exports = {
-    // route : handler
-    'get /zillow': 'index',
-    'get /zillow/:place': 'get',
-    'get /zillow/:place/drop': 'drop',
-    'get /zillow/:place/FeatureServer': 'featureserver',
-    'get /zillow/:place/FeatureServer/:layer': 'featureserver',
-    'get /zillow/:place/FeatureServer/:layer/:method': 'featureserver',
-    'get /zillow/:place/max_price/:max_price': 'get',
-    'get /zillow/:place/max_price/:max_price/drop': 'drop',
-    'get /zillow/:place/max_price/:max_price/FeatureServer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/FeatureServer/:layer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/FeatureServer/:layer/:method': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms': 'get',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/drop': 'drop',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/FeatureServer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/FeatureServer/:layer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/FeatureServer/:layer/:method': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms': 'get',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/drop': 'drop',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/FeatureServer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/FeatureServer/:layer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/FeatureServer/:layer/:method': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/sqft/:sqft': 'get',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/sqft/:sqft/drop': 'drop',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/sqft/:sqft/FeatureServer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/sqft/:sqft/FeatureServer/:layer': 'featureserver',
-    'get /zillow/:place/max_price/:max_price/bedrooms/:bedrooms/bathrooms/:bathrooms/sqft/:sqft/FeatureServer/:layer/:method': 'featureserver'
+var Combinatorics = require('js-combinatorics').Combinatorics;
+
+var actions = {
+    'drop': 'drop',
+    'FeatureServer': 'featureserver',
+    'Featureserver/:layer': 'featureserver',
+    'Featureserver/:layer/:method': 'featureserver'
 };
+var base = '/zillow/:place';
+var parameters = [
+'price',
+'bedrooms',
+'bathrooms',
+'sqft'
+];
+
+var combinations = Combinatorics.permutationCombination(parameters).toArray();
+
+var endpoints = [];
+endpoints.push(base);
+
+combinations.forEach(function(params){
+    if (params.length > 1){
+        var stub = '';
+        params.forEach(function(param){
+            stub += param + '/:' + param + '/';
+        });
+        endpoints.push(stub);
+    } else {
+        endpoints.push(params[0] + '/:' + params[0]);
+    }
+});
+console.log(endpoints);
+
+var routes = {
+    'get /zillow': 'index'
+};
+
+endpoints.forEach(function(endpoint){
+    console.log(endpoint);
+    routes['get ' + base + endpoint.slice(0,-1)] = 'get';
+    Object.keys(actions).forEach(function(action){
+        routes['get ' + base + '/' + endpoint + action] = actions[action];
+    });
+});
+
+console.log(Object.keys(routes));
+module.exports = routes;
 
